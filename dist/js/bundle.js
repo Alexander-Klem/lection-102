6c2251ae327a3205141b3eff5de96ef1f357a6de
Александр Клемято <a.klemyato@rsm.by>
2024-11-29 13:54:06 +0300

    Забыл добавить bundle)

---
+++ dist/js/bundle.js
@@ -0,0 +1,25 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/script.js":
+/*!**************************!*\
+  !*** ./src/js/script.js ***!
+  \**************************/
+/***/ (() => {
+
+throw new Error("Module parse failed: Unexpected token (1:25)\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\n> import { one, two } from `./main`;\n| \n| console.log(`${one} and ${two}`);");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/js/script.js"]();
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
